51cc25c05841da6e796264f53d6000eb39a21944
Wallmus <49403887+Wallmus@users.noreply.github.com>
2026-02-12 11:39:37 -0500

    whoops forgot updated sql file

--- lahman_baseball_mw.sql
+++ lahman_baseball_mw.sql
@@ -5,6 +5,39 @@ SELECT MIN(year) AS min_year,
 FROM homegames;
 
 -- 11. Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question. As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
+	-- Yes! I think so!
+-- * JUST WINS *
+SELECT teamid
+	, SUM(w) AS total_wins
+FROM teams
+WHERE yearid > 2000
+GROUP BY teamid
+ORDER BY total_wins DESC NULLS LAST;
+-- * JUST SALARIES *
+SELECT teamid, SUM(salary)::numeric::money AS salary
+FROM salaries
+GROUP BY teamid
+ORDER BY salary DESC;
+-- * WINS & SALARIES *
+WITH wins_per_team AS (
+	SELECT teamid
+		, SUM(w) AS total_wins
+	FROM teams
+	WHERE yearid > 2000
+	GROUP BY teamid
+	ORDER BY total_wins DESC NULLS LAST
+)
+SELECT wins_per_team.teamid, total_wins, SUM(salary)::numeric::money AS team_salary
+FROM wins_per_team
+INNER JOIN salaries
+	ON wins_per_team.teamid = salaries.teamid
+WHERE salaries.yearid > 2000
+GROUP BY wins_per_team.teamid, total_wins
+ORDER BY total_wins DESC;
+
+
+-- 12. In this question, you will explore the connection between number of wins and attendance.
+	-- Does there appear to be any correlation between attendance at home games and number of wins?
 SELECT teamid
 	, SUM(CASE WHEN lgwin='Y' THEN 1
 		WHEN wcwin='Y' THEN 1
@@ -14,18 +47,13 @@ SELECT teamid
 FROM teams
 GROUP BY teamid
 ORDER BY wins DESC NULLS LAST;
-
-SELECT teamid, SUM(salary)::numeric::money AS salary
-FROM salaries
-GROUP BY teamid
-ORDER BY salary DESC;
-
-SELECT *
-FROM teams;
-
--- 12. In this question, you will explore the connection between number of wins and attendance.
-	-- Does there appear to be any correlation between attendance at home games and number of wins?
-	
+-- 
+SELECT team, SUM(attendance) AS total_attend
+FROM homegames
+GROUP BY team
+ORDER BY total_attend DESC;
 	-- Do teams that win the world series see a boost in attendance the following year? What about teams that made the playoffs? Making the playoffs means either being a division winner or a wild card winner.
+	SELECT *
+	FROM teams;
 
 -- 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
